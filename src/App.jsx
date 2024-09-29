7bf839ed0000ae3f42c799282b78b027ebe35982
VirendraRaut <virendraraut1306@gmail.com>
2024-09-29 23:43:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar";
+import NewsBoard from "./Components/NewsBoard";
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBoard/>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const apiKey = import.meta.env.VITE_API_KEY;
+      console.log(apiKey); // Check if the API key is being correctly loaded
+      const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.articles) {
+        setArticles(data.articles);
+      }
+    };
+
+    fetchArticles();
+  }, []); // Run once on component mount
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.length > 0 ? (
+        articles.map((news, index) => (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        ))
+      ) : (
+        <p>No news articles available.</p>
+      )}
+    </div>
+  );
+};
+
+export default NewsBoard;
