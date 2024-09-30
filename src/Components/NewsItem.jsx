ace090c77caa78a8cd91106e918701af3d48dbab
VirendraRaut <virendraraut1306@gmail.com>
2024-09-30 20:53:48 +0530

    change

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,6 +1,6 @@
 const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card" style={{maxWidth:"345px"}}>
+    <div className="card" style={{ maxWidth: "345px" }}>
       <img src={src} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
