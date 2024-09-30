59057c17e65df6343ccaae0d4f7fb96358220784
VirendraRaut <virendraraut1306@gmail.com>
2024-09-30 21:55:33 +0530

    making theme dark and text color to white

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,6 +1,6 @@
 const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card" style={{ maxWidth: "345px" }}>
+    <div className="card bg-dark text-light mb-3 d-inline-block" style={{ maxWidth: "400px" }}>
       <img src={src} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
