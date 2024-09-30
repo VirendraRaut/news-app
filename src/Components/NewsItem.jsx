30a6ff45cfc44d3e79a2f11324f9517faa7a21b8
VirendraRaut <virendraraut1306@gmail.com>
2024-09-30 21:59:23 +0530

    giving some padding

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -1,6 +1,9 @@
 const NewsItem = ({ title, description, src, url }) => {
   return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-4 mx-4" style={{ maxWidth: "365px" }}>
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-4 mx-4 px-2 py-2"
+      style={{ maxWidth: "365px" }}
+    >
       <img src={src} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
